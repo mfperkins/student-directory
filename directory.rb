2e72867792989b46e7d1e7fa0923e0cd3c61c354
Matthew Perkins <mfperkins@gmail.com>
2016-07-26 10:54:08 +0100

    Refactored initial code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 # first we print the list of students
 
 puts "The students of Awesomeness Academy"
@@ -15,7 +17,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 # finally, we print the total number of students
-print "Overall, we have "
-# it's important that print() doesn't add new line characters
-print 11
-puts " great students."
+print "Overall, we have #{student_count} great students."
